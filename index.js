6c83ef839c897f7c34ce9f5a722a0c3c7625f903
Michael <mdlevick@gmail.com>
2019-12-03 12:10:04 -0600

    added vars and listen

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const express = require("express"); 
+
+const db = require("./data/hubs-model.js"); 
+
+const server = express();
+
+server.use(express.json()); 
+
+
+
+
+
+
+const port = 4000;
+server.listen(port, () =>
+  console.log(`\n ** API running on port ${port} **\n`)
+);
